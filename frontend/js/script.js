29eb9733b78c4d209114c11c3065efd2882d2eac
Rafael Santos de Araujo <rafaelsantosdearaujo@gmail.com>
2023-01-21 17:55:58 -0300

    implementando comunicação inicial com api

---
+++ frontend/js/script.js
@@ -0,0 +1,19 @@
+const fecthTasks = async () => {
+
+    const response = await fetch('http://localhost:3333/tasks');
+    const tasks = await response.json()
+    return tasks;
+
+}
+
+const createElement = (tag)=> {
+    const element = document.createElement(tag);
+    return element;
+
+}
+const createRow= (task) => {
+    const tr = document.createElement('tr');
+
+}
+
+
